8e7492f2fdc94dd20ed0a5b743353c593448a218
Kojin Oshiba <kojin.oshiba@gmail.com>
2018-10-03 16:20:05 -0400

    add cluster scripts

---
+++ main.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -o master.%j.txt
+#SBATCH -e master.%j.err
+#SBATCH -t 05:01:00
+#SBATCH --mem 1000
+#SBATCH -J adaptive
+#SBATCH -p shared
+#SBATCH --account=ysinger_group
+
+ks=(60 80 100 150 200 250 300 400 500)
+rs=(4 6 8 10 12)
+fraction_to_drops=(0.05 0.10 0.15 0.18 0.20 0.30 0.40 0.50)
+audios=('alexa' 'piano' 'marshmello')
+
+for k in ${ks[@]}
+do
+for fraction_to_drop in ${fraction_to_drops[@]}
+do
+for r in ${rs[@]}
+do
+for audio in ${audios[@]}
+do
+./main_helper.sh \
+$fraction_to_drop \
+$k \
+$r \
+$audio \
+
+done
+done
+done
+done
---
+++ main_helper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -o val.%j.txt
+#SBATCH -e val.%j.err
+#SBATCH -t 5-05:01:00
+#SBATCH --mem 999
+#SBATCH -J adaptive
+#SBATCH -p shared
+#SBATCH --account=ysinger_group
+
+python SpeechDenoise_full4.py \
+--fraction_to_drop=${1} \
+--k=${2} \
+--r=${3} \
+--audio=${4}
